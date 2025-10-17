6a94596fe2fea2914ab2841589279469cd8b8df0
Shalini  V <105047114+shaal123@users.noreply.github.com>
2025-10-17 22:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const byteSize = (str) => {
   // write your code here
+	function byteSize(str) {
+  return new Blob([str]).size;
+}
+
+// Example test cases:
+console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요'));   // 15
+console.log(byteSize(''));            // 0
 };
 
 // Do not change the code below
